5ed909e498140dbd9d0529ae4e4637c2ae025a0b
Chiken <adrivelazquez97@gmail.com>
2023-03-20 09:46:52 +0000

    Eslint fixing

--- src/redux/actions/movieActions.ts
+++ src/redux/actions/movieActions.ts
@@ -1,8 +1,3 @@
-// /* eslint-disable @typescript-eslint/no-unsafe-argument */
-// /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-// /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-// /* eslint-disable @typescript-eslint/no-unsafe-call */
-
 import {
 	SET_MOVIES,
 	SET_MOVIE_DETAIL,
