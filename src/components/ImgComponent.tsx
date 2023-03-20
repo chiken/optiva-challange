b4bcdede5c87b2c6416806803e034c44f3250d15
Chiken <adrivelazquez97@gmail.com>
2023-03-20 21:29:40 +0000

    Img not available backup

--- src/components/ImgComponent.tsx
+++ src/components/ImgComponent.tsx
@@ -1,7 +1,11 @@
 import { type ImgProps } from "../types";
 
+import Img from "../assets/img-no-available.jpeg";
+
 const { REACT_APP_TMDB_IMG_BASE_URL: baseURL = "" } = process.env;
 
 export const ImgComponent = ({ path, className }: ImgProps): JSX.Element => {
-	return <img className={className} src={`${baseURL}${path}`} alt={path} />;
+	const img = path === null || path.length === 0 ? Img : `${baseURL}${path}`;
+
+	return <img className={className} src={img} alt={path} />;
 };
